892b4e292f226872072d5aaa0859ea7873c5c7e8
Dianf333 <124247289+Dianf333@users.noreply.github.com>
2023-02-02 17:06:42 +0800

    Add files via upload

---
+++ text1/text1/源.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+struct book
+{
+	char name[14];
+	short price;
+};;
+int main()
+{
+	struct book b1 = { "C”Ô—‘≥Ã–Ú…Ëº∆",55 };
+}
---
+++ text1/text1/源1.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int dog = 114514;
+int dio(int x,int y)
+{
+	int z = x - y;
+	return z;
+}
